a1033a84635f568508615727a685f167951d454c
RaotRus <raot_rus@hotmail.com>
2020-12-17 19:53:02 +0300

    предподготовка

--- Arena_Console/obj/Debug/netcoreapp3.1/Arena_Console.AssemblyInfo.cs
+++ Arena_Console/obj/Debug/netcoreapp3.1/Arena_Console.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arena_Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.1.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arena_Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arena_Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
