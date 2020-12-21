5346a2478f241687407fdb73b4e5dc4154747e86
RaotRus <raot_rus@hotmail.com>
2020-12-22 00:34:36 +0300

    Разделил фаилы и подготовил скелет под меню, осталось его добавить

--- Enumy.cs
+++ Enumy.cs
@@ -1,55 +1,43 @@
 using System;
 class Enumy{
     public string name;
-    private int _hp {get;set;} 
-        public int HP{
-            get{ return _hp; }
-        }
-    private int _shield {get;set;}
-        public int Shield{
-            get{ return _shield;}
-        } 
-    private int _koshel{get;set;}
-        public int Koshel{
-            get{ return _koshel; }
-        }  
-    private int streng {get;set;}
-    private int[] weapon = new int[2];
-
+    private int HP;
+    private int streng;
+    private int shield;
+    private int koshel;
+    //Боевые
     public int Attack(int[] weapon, int streng){
         Random rand = new Random();
         int  result = streng + rand.Next(weapon[0],weapon[1]);
         return result;
     }
-    public int Deffend(int streng, int shield){
-        return streng+shield;
-    }
-    public void Hill(int hill){
-        _hp+=hill;
-    }
-    //Передать управление и решение о покупки в класс магазина, а через перса передавать значения
-    public bool Bay(int price){
-        if (_koshel>=price){ 
-            _koshel-=price;
-            return true;}
-        else{
-            Console.WriteLine("Кошелек маловат!");
-            return false;
+    public int Deffend(int streng, int shield) => streng+shield;   
+    public void Hill(int hill) => HP+=hill;
+    
+    //Магазин
+    public int GiveMoney(int Price){
+        if (Price>koshel){
+            return 0;
+        }else{
+            koshel-=Price;
         }
+        return Price;
     }
+    public void TakeMoney(int Prise) => koshel+=Prise;
+
     //Конструктор для игрока
     public Enumy(){
-        _hp = 15;
+        HP = 15;
         streng = 10;
-        _shield = 2;
-        _koshel = 25;
+        shield = 2;
+        koshel = 25;
     }
     //Конструктор для врага
     public Enumy(int LvL){
         Random rnd = new Random();//На сумму выйгрыша
-        _hp = 10 + (5*LvL);
+        HP = 10 + (5*LvL);
         streng = 10;
-        _shield = 2*LvL;
-        _koshel = 2+ rnd.Next(LvL,4*LvL);
+        shield = 0+5*LvL;
+        koshel = 2+ rnd.Next(LvL,4*LvL);
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,36 +3,18 @@
 namespace TestGame{
     class Program{
         static void Main(string[] args){
-            Enumy pers = new Enumy(); 
-            {
-            Console.WriteLine("Добро пожаловать на 22 вуликую битву на арене!");
-            Console.WriteLine("Назови свое имя путник!");
-            Console.Write("Имя: ");
-            pers.name = Console.ReadLine();
-            Console.WriteLine("Приветствуем, " + pers.name);
-            podgotovka(pers,0);
-            }
+            Enumy Pers = new Enumy();
+            //Приветсвие игрока
+            podgotovka(Pers,0);
         }
         static void podgotovka(Enumy pers, int LvL){
-            Console.WriteLine("Подготовься к битве!");
-            Console.WriteLine("Добро пожаловать в магазин!\nВыбери, что хочешь приобрести:");
-            Console.WriteLine("Бабки: "+pers.Koshel+"Жизни: "+pers.HP);
-            Shop sh = new Shop();
-            for(;;){
-                int i = sh.ShopMenu();
-                if (i==1 || i==2 || i==3){
-                    sh.PrintShopList(i,pers);
-                }
-                else if(i==4){
-                    battle(pers, LvL);
-                    break;
-                }else{
-                    Console.WriteLine("Не выбран пункт из списка");
-                }
-            }
+            Enumy Pers = pers;
+            //Процесс подготовки игрока: лечение и усиление
+            battle(Pers,LvL);
         }
         static void battle(Enumy pers,int LvL){
             Enumy Enum = new Enumy(LvL);
+            Enumy Pers = pers;
             //TODO: процесс битвы
             LvL++;
             podgotovka(pers, LvL);
